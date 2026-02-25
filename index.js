89525529613570abf9e2feb83cff01434f4d7e39
rakpatil <rakpatil@manh.com>
2026-02-25 15:02:39 +0530

    Include lib folder

--- index.js
+++ index.js
@@ -14,6 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require('./iframe/iframeSupport').applyIframeSupport();
+require('./lib/iframeSupport').applyIframeSupport();
 const { createConnection } = require('playwright/lib/mcp/index');
 module.exports = { createConnection };
