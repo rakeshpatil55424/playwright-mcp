c5a299205e5aabab64b82ab1c6605e4fa06f1f9b
rakpatil <rakpatil@manh.com>
2026-02-27 11:05:41 +0530

    Revoked iframe changes

--- index.js
+++ index.js
@@ -14,6 +14,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-require('/app/lib/iframeSupport.js').applyIframeSupport();
+
 const { createConnection } = require('playwright/lib/mcp/index');
 module.exports = { createConnection };
